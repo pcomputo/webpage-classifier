a9f6b06409ce8343aa0a9ec2f46c559b42663049
pcomputo <ahuja.pooja22@gmail.com>
2015-11-28 23:09:38 -0600

    Separated training from cleaning

--- datasetCleaner/cleanDataset.py
+++ datasetCleaner/cleanDataset.py
@@ -57,42 +57,14 @@ def review_to_words(directory):
 		
 		
 		fw.close()
-		print "Cleaned and vectorized:",page
+		print "Cleaned",page
 		print
 		#vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = None,  max_features = 5000)
 		
 		
 		#print meaningful_words
-	print "Total number of files cleaned and vectorized: ",count
-	vectorizer = TfidfVectorizer(token_pattern=r'\b[a-z]{2,}\b', max_df=0.3, min_df=2, sublinear_tf=True)
-	result = [(x,y) for x in os.walk(directory) for y in glob(os.path.join(x[0], '*.txt'))]
-	#print result
-	ls = []
-	y_train = []
-	for (cat,page) in result:
-		fp = open(page,"rb")
-		cleaned_data = fp.read()
-		fp.close()
-		ls.append(cleaned_data)
-		y_train.append(cat[0].split('/')[-2])
-		
-
-	print "Preparing vectorizer"	
-	X_train = vectorizer.fit_transform(ls)
-	print set(y_train)
-
-	# Initialize a Random Forest classifier with 100 trees
-	print "Preparing training"
-	forest = RandomForestClassifier(n_estimators = 100)
+	print "Total number of files cleaned: ",count
 	
-	print "Training in progress..."
-	forest = forest.fit(X_train.toarray(), y_train)
-	
-	print "Training completed!"
-	
-	print "Preparing classification"
-	result = forest.predict(test_data_features)
-	print "Classification completed!"
 	
 if __name__ == '__main__':
 	args = sys.argv[1:]
